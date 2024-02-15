25b64f26ab303acff92adb57fe9592c351d18c92
Digvijay Patil <digwijaypatil6521@gmail.com>
2024-02-15 14:09:29 +0530

    commit

--- Potd_9_2_24.py
+++ Potd_9_2_24.py
@@ -1,12 +1,9 @@
 '''
 Given a binary tree having n nodes. Check whether all of its nodes have the value equal to the sum of their child nodes. Return 1 if all the nodes in the tree satisfy the given properties, else it return 0.
 
- 
-
 For every node, data value must be equal to the sum of data values in left and right children. Consider data value as 0 for NULL child.  Also, leaves are considered to follow the property.
 
 Example 1:
-
 Input:
 Binary tree
        35
@@ -18,8 +15,8 @@ Output:
 1
 Explanation: 
 Here, every node is sum of its left and right child.
-Example 2:
 
+Example 2:
 Input:
 Binary tree
        1
