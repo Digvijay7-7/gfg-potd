997bd89be724be2e33e56295bc485a2a5ffe60ee
Digvijay Patil <digwijaypatil6521@gmail.com>
2024-02-15 14:06:29 +0530

    commit

--- a.py
+++ a.py
@@ -1,40 +1,90 @@
 '''
-You are presented with an undirected connected graph consisting of n vertices and connections between them represented by an adjacency matrix. Your objective is to determine whether it is possible to start traversing from a node, x, and return to it after traversing all the vertices at least once, using each edge exactly once.
+A critical connection refers to an edge that, upon removal, will make it impossible for certain nodes to reach each other through any path. You are given an undirected connected graph with v vertices and e edges and each vertex distinct and ranges from 0 to v-1, and you have to find all critical connections in the graph. It is ensured that there is at least one such edge present.
+
+Note: The answers may be presented in various potential orders. Each edge should be displayed in sorted order. For instance, if there's an edge between node 1 and node 2, it should be stored as (1,2) rather than (2,1). Additionally, it is expected that you store the edges in sorted order.
 
 Example 1:
-
-Input: 
-paths = 0 1 1 1 1
-        1 0 0 1 0
-        1 0 0 1 0
-        1 1 1 0 1
-        1 0 0 1 0
-Output: 1
+Input:
+https://prnt.sc/MtUgGwIJd_GU
+Output:
+0 1
+0 2
 Explanation: 
-One can visit the vertices in the following way:
-1->3->4->5->1->4->2->1 (assuming 1-based indexing)
-Here all the vertices has been visited and all
-paths are used exactly once.
+On removing edge (0, 1), you will not be able to
+reach node 0 and 2 from node 1. Also, on removing
+edge (0, 2), you will not be able to reach node 0
+and 1 from node 2.
+
 Example 2:
+Input:
+https://prnt.sc/Gd9l9MOtyc_Z
+Output:
+2 3
+Explanation:
+The edge between nodes 2 and 3 is the only
+Critical connection in the given graph.
+Your task:
+You dont need to read input or print anything. Your task is to complete the function criticalConnections() which takes the integer v denoting the number of vertices and an adjacency list adj as input parameters and returns  a list of lists containing the Critical connections in the sorted order.
 
-Input: 
-paths = 0 1 1 0
-        1 0 1 1
-        1 1 0 0
-        0 1 0 0
-Output: 0
-Explanation: 
-There exists no such vertex from which all the 
-vertices could be visited and all edges are used 
-exactly once.
-Your Task:
-You don't need to read or print anything. Your task is to complete the function isPossible() which takes adjacency matrix paths as an input parameter and returns 1 if it is possible to start traversing from a node, x, and come back to it after traversing all the vertices at least once, using each edge exactly once.
-
- Expected Time Complexity: O(n^2)
-Expected Space Compelxity: O(1)
+Expected Time Complexity: O(v + e)
+Expected Auxiliary Space: O(v)
 
 Constraints:
-1 <= n <= 100
-0 <= paths[i][j] <= 1
-Note: paths[i][j] = 0 where i == j or there exists no edge between i and j. paths[i][j] = 1 means there is a path between i to j.
+1 ≤ v, e ≤ 10^4
 '''
+
+#User function Template for python3
+
+class Solution:
+    def criticalConnections(self, V, adj):
+        disc = [-1 for i in range(V)]
+        low = [-1 for i in range(V)]
+        self.time = 0
+        bridges = []
+        def SCC(u, par = -1):
+            disc[u] = self.time
+            low[u] = self.time
+            self.time += 1
+            for v in adj[u]:
+                if v == par:
+                    continue
+                if disc[v] == -1:
+                    SCC(v, u)
+                    low[u] = min(low[u], low[v])
+                    if low[v] > disc[u]:
+                        if v > u:
+                            bridges.append([u, v])
+                        else:
+                            bridges.append([v, u])
+                else:
+                    low[u] = min(low[u], disc[v])
+        for i in range(V):
+            if disc[i] == -1:
+                SCC(i)
+        bridges.sort()
+        return bridges
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import sys
+sys.setrecursionlimit(10**6)
+
+if __name__ == '__main__':
+
+	T=int(input())
+	for i in range(T):
+		V, E = map(int, input().split())
+		adj = [[] for i in range(V)]
+		for _ in range(E):
+			u, v = map(int, input().split())
+			adj[u].append(v)
+			adj[v].append(u)
+		obj = Solution()
+		ans = obj.criticalConnections(V, adj)
+		for i in range(len(ans)):
+		    print(ans[i][0],ans[i][1])
+
+# } Driver Code Ends
